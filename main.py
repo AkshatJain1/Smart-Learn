a7e8811a095f335ca00a38668778b76eb653f64d
Akshat Jain <akshatj@berkeley.edu>
2020-05-05 17:36:26 -0500

    Added skill graph, demo-ready

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import turtle, random
 import time
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import random
+
 player = turtle.Turtle()
 player.penup()
 player.shape('turtle')
@@ -11,6 +18,40 @@ screen.bgcolor('teal')
 
 allTurtles = [player]
 bubbles = {}
+
+NUM_SOLUTIONS = 2
+
+''' data tracking containers '''
+firstMove = True
+startTime = 0
+
+# track periods where user is inactive
+inactivePeriods = []
+lastInactive = -1
+
+
+# not near turtles but switch right/left rapidly
+unnecessaryChange = {}
+unnecessaryChange["time"] = []
+unnecessaryChange["frequency"] = []
+
+# near turtles and switch right/left rapidly
+dodgeChange = {}
+dodgeChange["time"] = []
+dodgeChange["frequency"] = []
+
+lastLeft = 0
+lastRight = 0
+
+
+# incorrect answers because bad format. i.e. a 2,2,4 group
+# math being right doesn't matter
+incorrectAnswersDirections = 0
+# incorrect answers because math wasn't right
+# could also indicate bad planning
+incorrectAnswersMath = 0
+
+
 def setupTurtleBubble(t, number):
     t.speed(0)
     t.ht()
@@ -44,7 +85,7 @@ def stamp(t, number, color):
     t.forward(back)
 
 solutions = []
-while len(solutions) < 10:
+while len(solutions) < NUM_SOLUTIONS:
     num1 = random.randint(2,12)
     num2 = random.randint(2,12)
     sol = num1 * num2
@@ -61,26 +102,94 @@ turningRight = False
 turningLeft = False
 movingForward = False
 
+
+def roundDownNearest(x, factor):
+    return int(math.floor(x / float(factor))) * factor
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+
+def checkSuddenDirectionChange(lastOppDirection):
+    nearOtherTurtles = False
+    for turtle in bubbles:
+        if turtle.distance(player) <= 35:
+            nearOtherTurtles = True
+            break
+
+    def addFreq(lst):
+        timeBin = roundDownNearest(int(time.time() - startTime), 2)
+        if timeBin not in lst["time"]:
+            lst["time"].append(timeBin)
+            lst["frequency"].append(1)
+        else:
+            lst["frequency"][lst["time"].index(timeBin)] += 1
+
+    if nearOtherTurtles and time.time() - lastOppDirection < 1:
+        addFreq(dodgeChange)
+        return True
+    elif time.time() - lastOppDirection < 1:
+        addFreq(unnecessaryChange)
+        return True
+    return False
+
+def checkInactivity():
+    global lastInactive
+    if lastInactive != -1 and time.time() - lastInactive > 2:
+        timeBinStart = truncate(lastInactive - startTime, 1)
+        timeBinEnd = truncate(time.time() - startTime, 1)
+
+        inactivePeriods.append({"start": timeBinStart, "end": timeBinEnd})
+        lastInactive = -1
+
+def checkFirstMove():
+    global firstMove, startTime
+    if firstMove:
+        firstMove = False
+        startTime = time.time()
+
 def right():
-    global turningRight
+    global turningRight, lastLeft
     turningRight = True
+
+    checkFirstMove()
+    if checkSuddenDirectionChange(lastLeft):
+        lastLeft = 0
+    checkInactivity()
+
 def right_release():
-    global turningRight
+    global turningRight, lastInactive, lastRight
     turningRight = False
+    lastInactive = time.time()
+    lastRight = time.time()
 
 def left():
-    global turningLeft
+    global turningLeft, lastRight
     turningLeft = True
+
+    checkFirstMove()
+    if checkSuddenDirectionChange(lastRight):
+        lastRight = 0
+    checkInactivity()
+
 def left_release():
-    global turningLeft
+    global turningLeft, lastInactive, lastLeft
     turningLeft = False
+    lastInactive = time.time()
+    lastLeft - time.time()
 
 def forward():
     global movingForward
     movingForward = True
+
+    checkFirstMove()
+    checkInactivity()
+
 def forward_release():
-    global movingForward
+    global movingForward, lastInactive
     movingForward = False
+    lastInactive = time.time()
 
 screen.onkeypress(right, "Right")
 screen.onkeypress(left, "Left")
@@ -91,23 +200,24 @@ screen.onkeyrelease(forward_release, "Up")
 
 screen.listen()
 
-dummy = turtle.Turtle()
-dummy.penup()
-dummy.ht()
+
 
 activated = {}
 
-
-
 def isGroup():
+    global incorrectAnswersDirections, incorrectAnswersMath
     numbers = []
     for turtle in activated:
         numbers.append(activated[turtle])
-    print(sum(i <= 12 for i in numbers))
+
     if sum(i <= 12 for i in numbers) == 2 and (numbers[0] == numbers[1] * numbers[2] or numbers[1] == numbers[0] * numbers[2] or numbers[2] == numbers[1] * numbers[0]):
         return True
+    elif sum(i <= 12 for i in numbers) != 2:
+        incorrectAnswersDirections += 1
     else:
-        return False
+        incorrectAnswersMath += 1
+
+    return False
 
 def clearActivated():
     global activated
@@ -121,24 +231,28 @@ def resetActivated():
     for turtle in activated:
         stamp(turtle, activated[turtle], "red")
         screen.update()
-        time.sleep(0.3)
-    time.sleep(1)
+        time.sleep(0.2)
+    time.sleep(0.5)
     for turtle in activated:
         stamp(turtle, activated[turtle], "white")
         screen.update()
     activated = {}
     player.goto(0,0)
 
+score = 0
+dummy = turtle.Turtle()
+dummy.penup()
+dummy.ht()
 
-while True:
+while score < NUM_SOLUTIONS:
     screen.update()
     dummy.forward(1)
     if movingForward:
-        player.forward(0.08 + (0.02 if turningLeft or turningRight else 0))
+        player.forward(0.1 + (0.02 if turningLeft or turningRight else 0))
     if turningRight:
-        player.right(0.08)
+        player.right(0.1)
     if turningLeft:
-        player.left(0.08)
+        player.left(0.1)
 
     for turtle in bubbles:
         if player.distance(turtle) < 25 and turtle not in activated:
@@ -146,5 +260,88 @@ while True:
             activated[turtle] = bubbles[turtle]
             if len(activated) == 3 and isGroup():
                 clearActivated()
+                score += 1
             elif len(activated) == 3:
                 resetActivated()
+
+endTime = time.time()
+timeTaken = endTime - startTime
+
+player.goto(0,0)
+player.write("You WON!", font=("arial", 24, "normal"))
+player.ht()
+screen.update()
+time.sleep(3)
+
+
+periodStatus = {'time': [], 'active': []}
+start = 0
+for period in inactivePeriods:
+    periodStart = int(period["start"] * 10)
+    periodEnd = int(period["end"] * 10)
+
+    for i in range(start, periodStart):
+        periodStatus["time"].append(i/10.0)
+        periodStatus["active"].append(1)
+    for i in range(periodStart, periodEnd):
+        periodStatus["time"].append(i/10.0)
+        periodStatus["active"].append(0)
+
+    start = periodEnd
+
+for i in range(start, int(truncate(timeTaken, 1) * 10)):
+        periodStatus["time"].append(i/10.0)
+        periodStatus["active"].append(1)
+
+print("Period Status: ")
+print(periodStatus)
+df = pd.DataFrame(data = periodStatus)
+sns.lineplot(x="time", y="active", data=df)
+plt.savefig('Period_Status.png')
+plt.clf()
+
+for i in range(0, int(timeTaken), 2):
+    if i not in unnecessaryChange["time"]:
+        unnecessaryChange["time"].append(i)
+        unnecessaryChange["frequency"].append(0)
+print("UNNECESSARY CHANGES: ")
+print(unnecessaryChange)
+df = pd.DataFrame(data = unnecessaryChange)
+sns.lineplot(x="time", y="frequency", data=df)
+plt.savefig('Unnecessary.png')
+plt.clf()
+
+for i in range(0, int(timeTaken), 2):
+    if i not in dodgeChange["time"]:
+        dodgeChange["time"].append(i)
+        dodgeChange["frequency"].append(0)
+print("DODGE CHANGES: ")
+print(dodgeChange)
+df = pd.DataFrame(data = dodgeChange)
+sns.lineplot(x="time", y="frequency", data=df)
+plt.savefig('Dodge.png')
+plt.clf()
+
+print("INCORRECT DIRECTIONS")
+print(incorrectAnswersDirections)
+
+print("INCORRECT MATH")
+print(incorrectAnswersMath)
+
+labels = ["Openness", "Conscientious", "Neuroticism", "Agreeableness", "Extraversion"]
+stats = [random.randint(0,100) for i in range(5)]
+angles=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+stats=np.concatenate((stats,[stats[0]]))
+angles=np.concatenate((angles,[angles[0]]))
+
+fig=plt.figure()
+ax = fig.add_subplot(111, polar=True)
+ax.plot(angles, stats, 'o-', linewidth=2)
+ax.fill(angles, stats, alpha=0.25)
+ax.set_thetagrids(angles * 180/np.pi, labels)
+ax.set_title("Skill Chart")
+ax.grid(True)
+
+plt.savefig('radar.png')
+plt.clf()
+
